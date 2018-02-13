752514c425b932f8c782ae8971adb164e14d59e5
mrome007 <mrome007@ucr.edu>
2018-02-12 18:43:36 -0800

    Just some practice

---
+++ Assets/Scripts/GrannyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrannyController : MonoBehaviour 
+{
+    public GameObject GrannyObject;
+    private Animator grannyAnimator;
+
+    private void Start()
+    {
+        grannyAnimator = GrannyObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void ChangeAnimation(string trigger)
+    {
+        grannyAnimator.SetTrigger(trigger);
+    }
+}
